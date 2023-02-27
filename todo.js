19119e78bf3b8364e2471d6ec8ff52a384863165
Akhil Mali <maliakhil94@gmail.com>
2023-02-27 17:58:56 +0530

    done

--- todo.js
+++ todo.js
@@ -1,27 +1,4 @@
-// const newItem = document.getElementById("new-item");
-// const addBtn = document.getElementById("add-btn");
-// const list = document.getElementById("list");
 
-
-// addBtn.addEventListener("click", function(e) {
-
-//     e.preventDefault();
-
-//     const li = document.createElement("li");
-//     const span = document.createElement("span");
-//     span.textContent = newItem.value;
-//     li.appendChild(span);
-
-
-//     const deleteBtn = document.createElement("button");
-//     deleteBtn.textContent = "Delete";
-//     deleteBtn.addEventListener("click", function(){
-//         list.removeChild(li);
-//     });
-//     li.appendChild(deleteBtn);
-
-//     newItem.value = "";
-// })
 
 // get elements
 const newItem = document.getElementById("new-item");
